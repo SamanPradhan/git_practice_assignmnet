f07101948fff55e31f8a3548db5a00ea3ddf8659
Pravin <pravin08pawar@gmail.com>
2022-12-10 18:28:22 +0530

    prime code fixed

--- prime.js
+++ prime.js
@@ -1,6 +1,18 @@
 let l =90;
+let count=0;
+for(let i=1; i<=l; i++)
+{
+ if(l % i)
+ }		
+ count++;
+ }
+}
 
-for(let i=0; i<k; i++){
-	if(l % i){
-		console.log("it is prime")
-	}
+if(count==2)
+{
+  console.log("It is prime number");
+}
+else
+{
+  console.log("It is not a prime number);
+}
